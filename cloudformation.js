f1a026b098bcf7ea87d2cda160fa90ac3dc27c2a
Stefan Sundin <git@stefansundin.com>
2016-11-15 17:22:11 -0800

    Angular plz.

--- cloudformation.js
+++ cloudformation.js
@@ -1,6 +1,6 @@
 setInterval(function() {
   var checkbox = document.querySelector("input[name=CAPABILITY_IAM]");
   if (checkbox && !checkbox.checked) {
-    checkbox.checked = true;
+    checkbox.click();
   }
 }, 1000);
