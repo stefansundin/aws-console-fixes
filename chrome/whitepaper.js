b5352005c68f06e98d664c2fc1617b90d2d8e076
Stefan Sundin <git@stefansundin.com>
2017-03-28 20:35:26 -0700

    Only replace form completely if submit button contains "Download". Otherwise insert the button above the form.

--- chrome/whitepaper.js
+++ chrome/whitepaper.js
@@ -1,16 +1,22 @@
+// https://duckduckgo.com/?q=%22Please+complete+the+form+below+to+download+the%22+site%3Aaws.amazon.com&ia=web
 // https://aws.amazon.com/lambda/serverless-architectures-learn-more/
 // https://aws.amazon.com/lambda/serverless-architectures-learn-more/dl/
 
 var retURL = document.querySelector("input[name=retURL]");
 if (retURL) {
-  var node = retURL;
-  while (node.tagName != "FORM") {
-    node = node.parentNode;
-  }
   var btn = document.createElement("a");
   btn.href = retURL.value;
-  btn.className = "button btn-size-normal btn-non-block btn-gold";
-  btn.appendChild(document.createTextNode("Download"));
-  node.parentNode.appendChild(btn);
-  node.parentNode.removeChild(node);
+  var form = retURL.form;
+  var submit = form.querySelector("[type=submit]");
+  if (submit.textContent.indexOf("Download") != -1) {
+    btn.className = submit.className;
+    btn.appendChild(document.createTextNode(submit.textContent));
+    form.parentNode.appendChild(btn);
+    form.parentNode.removeChild(form);
+  }
+  else {
+    btn.className = "button btn-size-normal btn-non-block btn-gold";
+    btn.appendChild(document.createTextNode("Skip form"));
+    form.parentNode.insertBefore(btn, form);
+  }
 }
