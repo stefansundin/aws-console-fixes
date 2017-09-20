072dcd8820cb88975af8d0f51fc4a5247b74c0a8
Stefan Sundin <git@stefansundin.com>
2017-09-19 21:51:24 -0700

    Make upsell removal work in the new RDS console as well.

--- chrome/rds.js
+++ chrome/rds.js
@@ -3,7 +3,11 @@ var timer = setInterval(function() {
     var upsell = document.querySelector('#rds-dashboard-ad-aurora-mysql-launch-instance-btn');
     if (upsell) {
       clearInterval(timer);
-      var div = upsell.parentNode.parentNode.parentNode;
+      var div = upsell.parentNode;
+      while (div.parentNode.id != "rds-dashboard") {
+        div = div.parentNode;
+        if (!div.parentNode) return;
+      }
       div.parentNode.removeChild(div);
     }
   }
