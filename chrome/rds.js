aaa3d1e0c539c17974db9fca4928f9b765211973
Stefan Sundin <git@stefansundin.com>
2017-10-18 19:18:14 -0700

    Add code that helps you select more columns easier. This behavior is not really expected as a user, so it will not be merged to master.

--- chrome/rds.js
+++ chrome/rds.js
@@ -12,3 +12,39 @@ var timer = setInterval(function() {
     }
   }
 }, 200);
+
+setTimeout(function() {
+  if (document.body.classList.contains("awsui-mezzanine-overrides")) {
+    // New redesigned console
+    var timer2 = setInterval(function() {
+      if (window.location.hash.startsWith("#dbinstances:")) {
+        var checkbox = document.querySelector('.awsui-modal-__state-showing #awsui-toggle-3'); // Engine version
+        if (checkbox) {
+          clearInterval(timer2);
+          var div = checkbox.parentNode;
+          while (!div.classList.contains("awsui-table-content-selector-option")) {
+            div = div.parentNode;
+          }
+          console.log(div.textContent);
+          if (div.textContent == "Engine version") {
+            checkbox.click();
+            document.querySelector('awsui-modal .awsui-radio-native-input[value="100"]').click(); // 100 resources
+            document.querySelector('.awsui-table-preferences-confirm-button button').click();
+          }
+        }
+      }
+    }, 200);
+  }
+  else {
+    var timer2 = setInterval(function() {
+      if (window.location.hash.startsWith("#dbinstances:")) {
+        var checkbox = document.querySelector('.gwt-CheckBox[aria-label="Engine Version"] label');
+        if (checkbox) {
+          clearInterval(timer2);
+          checkbox.click();
+          document.querySelector('.dialogContent button').click();
+        }
+      }
+    }, 200);
+  }
+}, 1000);
