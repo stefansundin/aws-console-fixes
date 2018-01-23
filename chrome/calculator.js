6f7708dbbca6ad830bd514e1ee70f3c97cfdb2a6
Stefan Sundin <git@stefansundin.com>
2018-01-23 15:31:51 -0800

    calculator ProTip

--- chrome/calculator.js
+++ chrome/calculator.js
@@ -1,4 +1,8 @@
-// Just disable the free tier checkbox by default
+// Just disable the free tier checkbox by default.
+// ProTip: You can bookmark this URL to start with us-west-2 selected: https://calculator.s3.amazonaws.com/index.html#r=PDX
+// You can also select a service by default, e.g.: #r=PDX&s=S3
+// Regions: IAD, CMH, PDX, SFO, YUL, DUB, LHR, FRA, SIN, NRT, SYD, ICN, BOM, GRU, PDT, CDG
+// Services: EC2, S3, Route53, CloudFront, RDS, DynamoDB, ElastiCache, CloudWatch, SES, SNS, ElasticTranscoder, WorkSpaces, Zocalo (WorkDocs), DirectoryService, Redshift, Glacier, SQS, SWF, EMR, Kinesis, CloudSearch, Snowball, DirectConnect, VPN, SimpleDB, PremiumSupport
 
 var timer = setInterval(function() {
   var free_tier = document.querySelector('.GLOBAL_FREE_TIER input[type="checkbox"]');
