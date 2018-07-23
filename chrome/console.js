5ae0bf98b7c4fc3451c9afb1ab56a7f1bf64060e
Stefan Sundin <git@stefansundin.com>
2018-07-23 12:29:16 -0700

    Add a super convenient link that goes directly to your own IAM user, in the global navbar dropdown menu.

--- chrome/console.js
+++ chrome/console.js
@@ -1,5 +1,6 @@
 // Add data-region-id attr to current region to allow it to be shown in the dropdown.
 // The menu is rebuilt every time it is opened, so that means we need to use an interval and look up the element every time.
+// Add a link directly to your own user in the IAM console.
 
 var meta_region = document.querySelector("meta[name='awsc-mezz-region']");
 var current_region = document.querySelector("#regionMenuContent span.current-region");
@@ -9,4 +10,14 @@ if (meta_region && current_region) {
   setInterval(function() {
     document.querySelector("#regionMenuContent span.current-region").setAttribute("data-region-id", region);
   }, 200);
+}
+
+var username_div = document.querySelector("#awsc-login-display-name-user");
+if (username_div) {
+  // this element is not present when logged in to the root account
+  var username = username_div.textContent;
+  var username_link = document.createElement("a");
+  username_link.href = `https://console.aws.amazon.com/iam/home#/users/${username}`;
+  username_link.innerText = username;
+  username_div.replaceWith(username_link);
 }
