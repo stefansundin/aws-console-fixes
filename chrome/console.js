231f58161a7326b127254573200b5e45410593aa
Stefan Sundin <git@stefansundin.com>
2020-05-22 11:44:31 -0700

    Add a global shortcut to open the Services dropdown. Inspired by https://www.reddit.com/r/aws/comments/goi6og/is_there_a_way_to_open_the_services_menu_with_a/.

--- chrome/console.js
+++ chrome/console.js
@@ -1,10 +1,7 @@
 // Add data-region-id attr to current region to allow it to be shown in the dropdown.
 // The menu is rebuilt every time it is opened, so that means we need to use an interval and look up the element every time.
-// Add a link directly to your own user in the IAM console.
-
 var meta_region = document.querySelector("meta[name='awsc-mezz-region']");
 var current_region = document.querySelector("#regionMenuContent span.current-region");
-
 if (meta_region && current_region) {
   var region = meta_region.getAttribute("content");
   setInterval(function() {
@@ -12,6 +9,7 @@ if (meta_region && current_region) {
   }, 200);
 }
 
+// Add a link directly to your own user in the IAM console.
 // this element is not present when logged in to the root account
 var username_div = document.querySelector("#awsc-login-display-name-user");
 if (username_div) {
@@ -27,3 +25,36 @@ if (username_div) {
     username_div.replaceWith(username_link);
   }
 }
+
+// Global shortcuts
+document.body.addEventListener("keydown", e => {
+  // Press ` to open the Services dropdown and focus the search field. You can press the Escape key to close it when the search field is focused. Pressing ` again when the search field is not focused will close the dropdown.
+  const nav = document.getElementById("nav-servicesMenu");
+  const input = document.getElementById("awsc-services-search-autocomplete");
+  const dropdown = document.getElementById("servicesMenuContent");
+  if (!dropdown || !nav || !input) {
+    return;
+  }
+
+  if (e.target == input && e.key == "Escape") {
+    input.blur();
+    nav.click();
+    e.preventDefault();
+    return;
+  }
+  if (e.target.tagName == "INPUT" || e.target.tagName == "TEXTAREA") {
+    return;
+  }
+  if (e.key == "`") {
+    const hidden = (dropdown.style.display == "none");
+    nav.click();
+    if (hidden) {
+      input.focus();
+    }
+    else {
+      input.blur();
+    }
+    e.preventDefault();
+    return;
+  }
+});
