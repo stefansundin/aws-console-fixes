52cc48816adb5358ee6c85efb102a29ebc4bc966
Stefan Sundin <git@stefansundin.com>
2024-03-10 16:45:54 -0700

    Add a button to the popup to temporarily deregister all running scripts.

--- chrome/popup/popup.js
+++ chrome/popup/popup.js
@@ -14,30 +14,20 @@ document.addEventListener('DOMContentLoaded', async () => {
     document.body.classList.add('firefox');
   }
 
-  const options = await getOptions();
-
-  if (options.theme === 'auto') {
-    window
-      .matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', (e) => {
-        document.documentElement.setAttribute(
-          'data-bs-theme',
-          e.matches ? 'dark' : 'light',
-        );
-      });
-  }
-
-  const theme = options.theme === 'auto' ? getSystemTheme() : options.theme;
-  document.documentElement.setAttribute('data-bs-theme', theme);
-
-  const currentStateParagraph = /** @type HTMLParagraphElement */ (
-    document.getElementById('currentState')
-  );
-  const numEnabledScripts = options.enabledContentScripts.length;
-  currentStateParagraph.textContent = `${numEnabledScripts} scripts are enabled.`;
-  if (numEnabledScripts === 1) {
-    currentStateParagraph.textContent =
-      currentStateParagraph.textContent.replace('scripts are', 'script is');
+  {
+    const options = await getOptions();
+    if (options.theme === 'auto') {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', (e) => {
+          document.documentElement.setAttribute(
+            'data-bs-theme',
+            e.matches ? 'dark' : 'light',
+          );
+        });
+    }
+    const theme = options.theme === 'auto' ? getSystemTheme() : options.theme;
+    document.documentElement.setAttribute('data-bs-theme', theme);
   }
 
   const openOptionsButton = /** @type HTMLButtonElement */ (
@@ -48,19 +38,68 @@ document.addEventListener('DOMContentLoaded', async () => {
     window.close();
   });
 
-  const granted = await isRequiredPermissionsGranted();
-  if (!granted) {
+  const toggleScriptsButton = /** @type HTMLButtonElement */ (
+    document.getElementById('toggleScripts')
+  );
+
+  const fixPermissionsButton = /** @type HTMLButtonElement */ (
+    document.getElementById('fixPermissions')
+  );
+  fixPermissionsButton.addEventListener('click', async () => {
+    const options = await getOptions();
+    const requiredPermissions = getRequiredPermissions(options);
+    chrome.permissions.request(requiredPermissions);
+    window.close();
+  });
+
+  async function refreshState() {
+    const options = await getOptions();
+    const registeredContentScripts =
+      await chrome.scripting.getRegisteredContentScripts();
+
+    const currentStateParagraph = /** @type HTMLParagraphElement */ (
+      document.getElementById('currentState')
+    );
+    currentStateParagraph.textContent = `${registeredContentScripts.length} scripts are registered.`;
+    if (registeredContentScripts.length === 1) {
+      currentStateParagraph.textContent =
+        currentStateParagraph.textContent.replace('scripts are', 'script is');
+    }
+
+    toggleScriptsButton.disabled = options.enabledContentScripts.length === 0;
+    toggleScriptsButton.textContent =
+      options.enabledContentScripts.length > 0 &&
+      registeredContentScripts.length === 0
+        ? 'Enable scripts again'
+        : 'Temporarily disable all scripts';
+
+    const granted = await isRequiredPermissionsGranted();
     const insufficientPermissionsAlert = /** @type HTMLDivElement */ (
       document.getElementById('insufficientPermissionsAlert')
     );
-    insufficientPermissionsAlert.classList.remove('d-none');
-    const fixPermissionsButton = /** @type HTMLButtonElement */ (
-      document.getElementById('fixPermissions')
+    insufficientPermissionsAlert.classList.toggle(
+      'd-none',
+      granted || registeredContentScripts.length === 0,
     );
-    fixPermissionsButton.addEventListener('click', () => {
-      const requiredPermissions = getRequiredPermissions(options);
-      chrome.permissions.request(requiredPermissions);
-      window.close();
-    });
   }
+
+  toggleScriptsButton.addEventListener('click', async () => {
+    try {
+      toggleScriptsButton.disabled = true;
+      const registeredContentScripts =
+        await chrome.scripting.getRegisteredContentScripts();
+      if (registeredContentScripts.length === 0) {
+        await chrome.runtime.sendMessage({ type: 'updateOptions' });
+      } else {
+        await chrome.scripting.unregisterContentScripts({
+          ids: registeredContentScripts.map((s) => s.id),
+        });
+      }
+      refreshState();
+    } finally {
+      toggleScriptsButton.disabled = false;
+    }
+  });
+
+  refreshState();
 });
